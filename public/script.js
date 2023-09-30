f0a512f8f0a4d72c1d356af765e322ce08933230
kuya-Ey <138701056+kuya-Ey@users.noreply.github.com>
2023-09-30 12:36:07 +0800

    adjustments on codes

--- public/script.js
+++ public/script.js
@@ -19,66 +19,66 @@ fetch('https://icanhazdadjoke.com/', {
 }
 
 let currentPage = 1;
-        const limit = 6; // Number of results per page
-        let searchTerm = '';
+    const limit = 6; // Number of results per page
+    let searchTerm = '';
 
-        // Function to fetch and display dad jokes
-        function fetchJokes(page) {
-            const jokeContainer = document.getElementById('joke-container');
+// Function to fetch and display dad jokes
+function fetchJokes(page) {
+    const jokeContainer = document.getElementById('joke-container');
 
-            // Make a GET request to the Dad Joke API
-            const apiUrl = `https://icanhazdadjoke.com/search?page=${page}&limit=${limit}&term=${searchTerm}`;
-            fetch(apiUrl, {
-                headers: {
-                    'Accept': 'application/json',
-                }
-            })
-                .then(response => response.json())
-                .then(data => {
-                    // Clear the previous jokes
-                    jokeContainer.innerHTML = '';
+    // Make a GET request to the Dad Joke API
+    const apiUrl = `https://icanhazdadjoke.com/search?page=${page}&limit=${limit}&term=${searchTerm}`;
+    fetch(apiUrl, {
+        headers: {
+            'Accept': 'application/json',
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            // Clear the previous jokes
+            jokeContainer.innerHTML = '';
 
-                    if (data.results && data.results.length > 0) {
-                        data.results.forEach(joke => {
-                            const jokeCard = document.createElement('div');
-                            jokeCard.classList.add('joke-card');
-                            jokeCard.innerHTML = `<p>${joke.joke}</p>`;
-                            jokeContainer.appendChild(jokeCard);
-                        });
-                    } else {
-                        jokeContainer.innerHTML = '<p>No jokes found.</p>';
-                    }
-
-                    document.getElementById('page').textContent = currentPage;
-                })
-                .catch(error => {
-                    console.error('Error fetching jokes:', error);
+            if (data.results && data.results.length > 0) {
+                data.results.forEach(joke => {
+                    const jokeCard = document.createElement('div');
+                    jokeCard.classList.add('joke-card');
+                    jokeCard.innerHTML = `<p>${joke.joke}</p>`;
+                    jokeContainer.appendChild(jokeCard);
                 });
-        }
-
-        // Function to handle next page
-        function nextPage() {
-            currentPage++;
-            fetchJokes(currentPage);
-        }
-
-        // Function to handle previous page
-        function prevPage() {
-            if (currentPage > 1) {
-                currentPage--;
-                fetchJokes(currentPage);
+            } else {
+                jokeContainer.innerHTML = '<p>No jokes found.</p>';
             }
-        }
 
-        // Function to handle search
-        function searchJokes() {
-            searchTerm = document.getElementById('searchTerm').value.trim();
-            currentPage = 1; // Reset to the first page when searching
-            fetchJokes(currentPage);
-        }
+                document.getElementById('page').textContent = currentPage;
+            })
+            .catch(error => {
+                console.error('Error fetching jokes:', error);
+            });
+    }
 
-        // Load initial jokes
+// Function to handle next page
+function nextPage() {
+    currentPage++;
+    fetchJokes(currentPage);
+}
+
+// Function to handle previous page
+function prevPage() {
+    if (currentPage > 1) {
+        currentPage--;
         fetchJokes(currentPage);
+    }
+}
+
+// Function to handle search
+function searchJokes() {
+    searchTerm = document.getElementById('searchTerm').value.trim();
+    currentPage = 1; // Reset to the first page when searching
+    fetchJokes(currentPage);
+}
+
+// Load initial jokes
+fetchJokes(currentPage);
 
  // JavaScript to trigger the modal when the link is clicked
 $(document).ready(function() {
